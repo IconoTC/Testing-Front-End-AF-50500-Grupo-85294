8b664875cae461e00a61d966fc74b3c75987c313
Alejandro Cerezo <alce65@hotmail.es>
2025-11-20 13:49:49 +0100

    Add playright project demo3.e2e

---
+++ projects/demo3-e2e/tests/example.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+const url = 'https://playwright.dev/';
+
+test.describe('Demo3 E2E tests', () => {
+    test('has title', async ({ page }) => {
+        await page.goto(url);
+
+        // Expect a title "to contain" a substring.
+        await expect(page).toHaveTitle(/Playwright/);
+    });
+
+    test('get started link', async ({ page }) => {
+        await page.goto(url);
+
+        // Click the get started link.
+        await page.getByRole('link', { name: 'Get started' }).click();
+
+        // Expects page to have a heading with the name of Installation.
+        await expect(
+            page.getByRole('heading', { name: 'Installation' })
+        ).toBeVisible();
+    });
+});
